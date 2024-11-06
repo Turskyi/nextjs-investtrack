96f6b49e9a9bb3bec333d8c61acf887a2aa26ce6
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2024-11-05 21:02:27 -0500

    test without all dynamics vlaues on details

--- src/app/investments/[slug]/InvestmentPage.tsx
+++ src/app/investments/[slug]/InvestmentPage.tsx
@@ -1,11 +1,14 @@
-import { formatDateForDisplay, formatMoney } from "@/lib/utils";
+import {
+  formatDateForDisplay,
+  // formatMoney,
+} from "@/lib/utils";
 import { Investment } from "@prisma/client";
 import {
   Banknote,
   Building,
   Globe2,
   LineChart,
-  TrendingUp,
+  // TrendingUp,
   Calendar,
 } from "lucide-react";
 import Image from "next/image";
@@ -16,73 +19,73 @@ interface InvestmentPageProps {
   investment: Investment;
 }
 
-async function fetchPriceChange(ticker: string) {
-  try {
-    const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/change?ticker=${ticker}`;
-    const res = await fetch(url);
-    if (!res.ok) throw new Error("Failed to fetch price change 😔.");
-    const data = await res.json();
-    return data.priceChange;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
+// async function fetchPriceChange(ticker: string) {
+//   try {
+//     const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/change?ticker=${ticker}`;
+//     const res = await fetch(url);
+//     if (!res.ok) throw new Error("Failed to fetch price change 😔.");
+//     const data = await res.json();
+//     return data.priceChange;
+//   } catch (error) {
+//     console.error(error);
+//     return null;
+//   }
+// }
 
-async function fetchChangePercentage(ticker: string) {
-  try {
-    const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/change-percentage?ticker=${ticker}`;
-    const res = await fetch(url);
-    if (!res.ok) throw new Error("Failed to fetch change percentage 😔.");
+// async function fetchChangePercentage(ticker: string) {
+//   try {
+//     const url = `${process.env.NEXT_PUBLIC_BASE_URL}/api/change-percentage?ticker=${ticker}`;
+//     const res = await fetch(url);
+//     if (!res.ok) throw new Error("Failed to fetch change percentage 😔.");
 
-    const data = await res.json();
-    return data.changePercentage;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
+//     const data = await res.json();
+//     return data.changePercentage;
+//   } catch (error) {
+//     console.error(error);
+//     return null;
+//   }
+// }
 
-async function fetchStockPrice(ticker: string, date?: Date | string | null) {
-  try {
-    // If a date is provided, format it as a string (YYYY-MM-DDTHH:MM:SS).
-    const formattedDate =
-      date instanceof Date
-        ? // This removes the milliseconds part.
-          date.toISOString().split(".")[0]
-        : date;
+// async function fetchStockPrice(ticker: string, date?: Date | string | null) {
+//   try {
+//     // If a date is provided, format it as a string (YYYY-MM-DDTHH:MM:SS).
+//     const formattedDate =
+//       date instanceof Date
+//         ? // This removes the milliseconds part.
+//           date.toISOString().split(".")[0]
+//         : date;
 
-    const url = formattedDate
-      ? `${process.env.NEXT_PUBLIC_BASE_URL}/api/finance?ticker=${ticker}&date=${formattedDate}`
-      : `${process.env.NEXT_PUBLIC_BASE_URL}/api/finance?ticker=${ticker}`;
+//     const url = formattedDate
+//       ? `${process.env.NEXT_PUBLIC_BASE_URL}/api/finance?ticker=${ticker}&date=${formattedDate}`
+//       : `${process.env.NEXT_PUBLIC_BASE_URL}/api/finance?ticker=${ticker}`;
 
-    const res = await fetch(url);
-    if (!res.ok) throw new Error("Failed to fetch stock price 😔.");
+//     const res = await fetch(url);
+//     if (!res.ok) throw new Error("Failed to fetch stock price 😔.");
 
-    const data = await res.json();
-    return data.currentPrice;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
+//     const data = await res.json();
+//     return data.currentPrice;
+//   } catch (error) {
+//     console.error(error);
+//     return null;
+//   }
+// }
 
-async function fetchExchangeRate(
-  fromCurrency: string,
-  toCurrency: string = "CAD",
-) {
-  try {
-    const res = await fetch(
-      `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`,
-    );
-    if (!res.ok) throw new Error("Failed to fetch exchange rate 😔.");
-    const data = await res.json();
-    return data.rates[toCurrency] || null;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
-}
+// async function fetchExchangeRate(
+//   fromCurrency: string,
+//   toCurrency: string = "CAD",
+// ) {
+//   try {
+//     const res = await fetch(
+//       `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`,
+//     );
+//     if (!res.ok) throw new Error("Failed to fetch exchange rate 😔.");
+//     const data = await res.json();
+//     return data.rates[toCurrency] || null;
+//   } catch (error) {
+//     console.error(error);
+//     return null;
+//   }
+// }
 
 export default async function InvestmentPage({
   investment: {
@@ -97,32 +100,32 @@ export default async function InvestmentPage({
     purchaseDate,
   },
 }: InvestmentPageProps) {
-  const currentPrice = await fetchStockPrice(ticker);
+  // const currentPrice = await fetchStockPrice(ticker);
   // Fetch price on purchase date.
-  const purchasePrice = await fetchStockPrice(ticker, purchaseDate);
+  // const purchasePrice = await fetchStockPrice(ticker, purchaseDate);
   const isPurchased = quantity > 0;
-  const totalValueCurrent = quantity * (currentPrice || 0);
-  const exchangeRate =
-    currency !== "CAD" ? await fetchExchangeRate(currency) : 1;
-  const totalValueCAD = totalValueCurrent * exchangeRate;
-  const totalValuePurchase = quantity * (purchasePrice || 0);
-  const totalValuePurchaseCAD = totalValuePurchase * exchangeRate;
+  // const totalValueCurrent = quantity * (currentPrice || 0);
+  // const exchangeRate =
+  //   currency !== "CAD" ? await fetchExchangeRate(currency) : 1;
+  // const totalValueCAD = totalValueCurrent * exchangeRate;
+  // const totalValuePurchase = quantity * (purchasePrice || 0);
+  // const totalValuePurchaseCAD = totalValuePurchase * exchangeRate;
 
-  const gainOrLoss = totalValueCurrent - totalValuePurchase;
-  const gainOrLossPercentage = totalValuePurchase
-    ? ((gainOrLoss / totalValuePurchase) * 100).toFixed(2)
-    : "0.00";
-  const gainOrLossCAD = totalValueCAD - totalValuePurchaseCAD;
-  const gainOrLossPercentageCAD = totalValuePurchaseCAD
-    ? ((gainOrLossCAD / totalValuePurchaseCAD) * 100).toFixed(2)
-    : "0.00";
+  // const gainOrLoss = totalValueCurrent - totalValuePurchase;
+  // const gainOrLossPercentage = totalValuePurchase
+  //   ? ((gainOrLoss / totalValuePurchase) * 100).toFixed(2)
+  //   : "0.00";
+  // const gainOrLossCAD = totalValueCAD - totalValuePurchaseCAD;
+  // const gainOrLossPercentageCAD = totalValuePurchaseCAD
+  //   ? ((gainOrLossCAD / totalValuePurchaseCAD) * 100).toFixed(2)
+  //   : "0.00";
 
   // Get the color for the current investment type.
   const typeColor = investmentTypeColors[type] || "text-gray-500";
 
-  const priceChange = await fetchPriceChange(ticker);
+  // const priceChange = await fetchPriceChange(ticker);
 
-  const changePercentage = await fetchChangePercentage(ticker);
+  // const changePercentage = await fetchChangePercentage(ticker);
 
   return (
     <section className="w-full grow space-y-5">
@@ -155,12 +158,15 @@ export default async function InvestmentPage({
               <Building size={16} className="shrink-0" />
               {stockExchange}
             </p>
-            <p className="flex items-center gap-1.5">
+
+            {/* test */}
+            {/* <p className="flex items-center gap-1.5">
               <Banknote size={16} className="shrink-0" />
               {currentPrice !== null
                 ? formatMoney(currentPrice, currency)
                 : "Price unavailable"}
-            </p>
+            </p> */}
+
             <p className="flex items-center gap-1.5">
               <Globe2 size={16} className="shrink-0" />
               {currency}
@@ -176,7 +182,8 @@ export default async function InvestmentPage({
               <Banknote size={16} className="shrink-0" />
               Quantity: {quantity}
             </p>
-            <p className="flex items-center gap-1.5">
+            {/* test */}
+            {/* <p className="flex items-center gap-1.5">
               <TrendingUp size={16} className="shrink-0" />
               Total Value (Current): {formatMoney(totalValueCurrent, currency)}
             </p>
@@ -185,13 +192,16 @@ export default async function InvestmentPage({
                 <TrendingUp size={16} className="shrink-0" />
                 Total Value (Current CAD): {formatMoney(totalValueCAD, "CAD")}
               </p>
-            )}
+            )} */}
+
             <p className="flex items-center gap-1.5">
               <Calendar size={16} className="shrink-0" />
               Purchase Date:{" "}
               {purchaseDate ? formatDateForDisplay(purchaseDate) : "N/A"}
             </p>
-            <p className="flex items-center gap-1.5">
+
+            {/* test */}
+            {/* <p className="flex items-center gap-1.5">
               <Banknote size={16} className="shrink-0" />
               Purchase Price:{" "}
               {purchasePrice ? formatMoney(purchasePrice, currency) : "N/A"}
@@ -222,11 +232,12 @@ export default async function InvestmentPage({
               <TrendingUp size={16} className="shrink-0" />
               Gain/Loss CAD: {formatMoney(gainOrLossCAD, "CAD")} (
               {gainOrLossPercentageCAD}%)
-            </p>
+            </p> */}
           </div>
         )}
         <div className="space-y-2 text-muted-foreground">
-          <p className="flex items-center gap-1.5">
+          {/* test */}
+          {/* <p className="flex items-center gap-1.5">
             {" "}
             <TrendingUp size={16} className="shrink-0" /> Price Change:{" "}
             {priceChange ? `${priceChange.toFixed(2)}%` : "N/A"}{" "}
@@ -237,7 +248,7 @@ export default async function InvestmentPage({
             {changePercentage !== null
               ? `${changePercentage.toFixed(2)}%`
               : "N/A"}
-          </p>
+          </p> */}
         </div>
       </div>
 
