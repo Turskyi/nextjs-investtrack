f3cbdf05c1b07e438784582072d8f27f87c3d177
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2024-11-17 01:13:32 -0500

    :zap: Adds database seeding script
    Introduces a script to populate the database with placeholder investment data for development and testing purposes.  The script uses Prisma ORM to interact with the database and handles potential errors gracefully.

---
+++ scripts/placeholder-data.js
@@ -0,0 +1,85 @@
+const placeholderInvestments = [
+  {
+    slug: "take-two-interactive-software-inc",
+    ticker: "TTWO",
+    type: "Software",
+    companyName: "TAKE-TWO INTERACTIVE SOFTWARE, INC",
+    stockExchange: "NASDAQ",
+    currency: "USD",
+    quantity: 0,
+    description: `
+      **About Take-Two Interactive**
+      
+      Take-Two Interactive is a leading developer, publisher, and marketer of entertainment for consumers around the world. 
+      Known for games like Grand Theft Auto and NBA 2K, Take-Two Interactive is an innovative player in the video game industry.
+    `,
+    isPurchased: false,
+  },
+  {
+    slug: "alphabet-inc-class-c",
+    ticker: "GOOG",
+    type: "Technology",
+    companyName: "Alphabet Inc Class C",
+    stockExchange: "NASDAQ",
+    currency: "USD",
+    quantity: 0,
+    description: `
+      **About Alphabet (Google)**
+      
+      Alphabet is the parent company of Google, specializing in various internet services and products. 
+      With its global reach and cutting-edge innovation, Alphabet is a tech giant driving the digital world.
+    `,
+    isPurchased: false,
+  },
+  {
+    slug: "apple-inc",
+    ticker: "AAPL",
+    type: "Technology",
+    companyName: "Apple Inc",
+    stockExchange: "NASDAQ",
+    currency: "USD",
+    quantity: 0,
+    description: `
+      **About Apple**
+      
+      Apple Inc. is a leading technology company known for its iconic products like the iPhone, iPad, and MacBook. 
+      With its innovative designs and strong brand, Apple continues to dominate the tech market.
+    `,
+    isPurchased: false,
+  },
+  {
+    slug: "microsoft-corp",
+    ticker: "MSFT",
+    type: "Technology",
+    companyName: "Microsoft Corp",
+    stockExchange: "NASDAQ",
+    currency: "USD",
+    quantity: 0,
+    description: `
+      **About Microsoft**
+      
+      Microsoft is a global leader in software development and cloud computing. 
+      The company is renowned for products like Windows, Office, and Azure, constantly pushing technological innovation.
+    `,
+    isPurchased: false,
+  },
+  {
+    slug: "nvidia-corp",
+    ticker: "NVDA",
+    type: "Semiconductors",
+    companyName: "NVIDIA Corp",
+    stockExchange: "NASDAQ",
+    currency: "USD",
+    quantity: 0,
+    description: `
+      **About NVIDIA**
+      
+      NVIDIA is a pioneer in AI computing and gaming hardware. Known for its powerful GPUs, NVIDIA is transforming industries from gaming to autonomous vehicles.
+    `,
+    isPurchased: false,
+  },
+];
+
+module.exports = {
+  placeholderInvestments,
+};
