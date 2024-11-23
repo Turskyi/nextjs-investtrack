8ed47b2e848f19673353a63269569e6f003d5045
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2024-11-23 13:34:55 -0500

    :zap: Improves investment page subtitle
    Adds a dynamic subtitle to the investments page that reflects the currently applied filters.  The subtitle provides more context to the user about the displayed investment results.

--- src/app/investments/page.tsx
+++ src/app/investments/page.tsx
@@ -42,6 +42,46 @@ function getTitle({
   return `${titlePrefix}${titleSuffix}`;
 }
 
+function getSubtitle({
+  q,
+  type,
+  currency,
+  stockExchange,
+  isPurchased,
+}: InvestmentFilterValues) {
+  // Define a subtitle based on the provided filter values
+  if (
+    !q &&
+    !type &&
+    !currency &&
+    !stockExchange &&
+    typeof isPurchased !== "boolean"
+  ) {
+    return "Browse all available investments.";
+  }
+
+  // Conditional subtitle based on filters
+  if (q) {
+    return `Explore ${q} investment opportunities.`;
+  }
+
+  if (type) {
+    return `Discover various ${type} opportunities.`;
+  }
+
+  if (stockExchange) {
+    return `Find investments listed on ${stockExchange}.`;
+  }
+
+  if (typeof isPurchased === "boolean") {
+    return isPurchased === true
+      ? "View all purchased investments."
+      : "See investments you haven't purchased yet.";
+  }
+
+  return ""; // Default case, if no filters are applied
+}
+
 export function generateMetadata({
   searchParams: { q, type, currency, stockExchange, isPurchased },
 }: InvestmentsPageProps): Metadata {
@@ -78,12 +118,10 @@ export default async function InvestmentsPage({
   };
 
   return (
-    <main className="m-auto my-10 max-w-5xl space-y-10 px-3 flex-grow">
+    <main className="m-auto my-10 max-w-5xl flex-grow space-y-10 px-3">
       <div className="space-y-5 text-center">
         <H1>{getTitle(filterValues)}</H1>
-        <p className="text-muted-foreground">
-          Find your next investment opportunity.
-        </p>
+        <p className="text-muted-foreground">{getSubtitle(filterValues)}</p>
       </div>
       <section className="flex flex-col gap-4 md:flex-row">
         <InvestmentFilterSidebar defaultValues={filterValues} />
